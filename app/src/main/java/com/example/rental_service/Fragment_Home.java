b44f0cef0bd72da9b16b5b06a1cbe1602450835e
NadudAn <dsd932@naver.com>
2022-07-16 18:27:49 +0900

    홈 프레그먼트 레이아웃 틀 제작

--- app/src/main/java/com/example/rental_service/Fragment_Home.java
+++ app/src/main/java/com/example/rental_service/Fragment_Home.java
@@ -4,21 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment_Home extends Fragment {
+    TextView notice;
+    Button btnPlace, btnStuff;
 
     public Fragment_Home() {
         // Required empty public constructor
     }
 
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        notice = (TextView) view.findViewById(R.id.notice);
+        btnPlace = (Button) view.findViewById(R.id.btnPlace);
+        btnStuff = (Button) view.findViewById(R.id.btnStuff);
+
+        btnPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnStuff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
     }
 }
