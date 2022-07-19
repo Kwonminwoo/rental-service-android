c0a6b09063b0a99d366eeb5d4798532e26261797
NadudAn <dsd932@naver.com>
2022-07-19 21:35:46 +0900

    Update Home Fragment layout && Upload icons and drawable files

--- app/src/main/java/com/example/rental_service/Fragment_Home.java
+++ app/src/main/java/com/example/rental_service/Fragment_Home.java
@@ -5,16 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class Fragment_Home extends Fragment {
-    TextView notice;
-    Button btnPlace, btnStuff;
+    TextView textNotice;
+    ImageButton btnPlace, btnStuff;
+    CardView cdView_loanable, cdView_loan_requested, cdView_rentable;
+    LinearLayout notice;
 
     public Fragment_Home() {
         // Required empty public constructor
@@ -25,9 +30,15 @@ public class Fragment_Home extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        notice = (TextView) view.findViewById(R.id.notice);
-        btnPlace = (Button) view.findViewById(R.id.btnPlace);
-        btnStuff = (Button) view.findViewById(R.id.btnStuff);
+        textNotice = (TextView) view.findViewById(R.id.textNotice);
+        btnPlace = (ImageButton) view.findViewById(R.id.btnPlace);
+        btnStuff = (ImageButton) view.findViewById(R.id.btnStuff);
+
+        cdView_loanable = (CardView) view.findViewById(R.id.cdView_loanable_stuff);
+        cdView_loan_requested = (CardView) view.findViewById(R.id.cdView_loan_requested_stuff);
+        cdView_rentable = (CardView) view.findViewById(R.id.cdView_rentable_place);
+
+        notice = (LinearLayout) view.findViewById(R.id.notice);
 
         btnPlace.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +48,27 @@ public class Fragment_Home extends Fragment {
         });
 
         btnStuff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cdView_loanable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cdView_loan_requested.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cdView_rentable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
