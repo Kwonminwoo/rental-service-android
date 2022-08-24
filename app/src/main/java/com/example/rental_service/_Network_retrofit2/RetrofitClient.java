2c44701dee29a2dc924a7956d82ab60a143b29e3
maria2580 <marin6670@gmail.com>
2022-08-24 10:26:18 +0900

    api 서버 url 제거. 필요시 요청

--- app/src/main/java/com/example/rental_service/_Network_retrofit2/RetrofitClient.java
+++ app/src/main/java/com/example/rental_service/_Network_retrofit2/RetrofitClient.java
@@ -3,12 +3,15 @@ package com.example.rental_service._Network_retrofit2;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.net.URL;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 //warning: no need to be edited more.
 public class RetrofitClient {
-    private static final String BASE_URL = "http://3.35.84.108.8081/";
+
+    private static final String BASE_URL = URL_holder.getURL();
     public static Retrofit getInstance(){
         Gson gson = new GsonBuilder().setLenient().create();
         return new Retrofit.Builder()
